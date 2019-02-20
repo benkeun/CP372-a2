02bd76b86ffbd474ca39715a7d4094eac7cf3780
Ben Keunen <43147054+benkeun@users.noreply.github.com>
2019-02-20 17:10:11 -0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -10,6 +10,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.util.*;
 import javax.swing.*;
@@ -19,15 +20,20 @@ public class Server extends JFrame implements ActionListener {
     static JPanel clientPanel = new JPanel();
     static JButton connectButton = new JButton("CONNECT");
     static JButton disconnectButton = new JButton("DISCONNECT");
+    static JButton transferButton = new JButton("TRANSFER");
     static JLabel IPLabel = new JLabel("IP Address");
     static JLabel ackPortLabel = new JLabel("ACK Port Number");
     static JLabel dataPortLabel = new JLabel("Data Port Number");
     static JLabel fileNameLabel = new JLabel("File Name");
-    static JTextField IPField = new JTextField("");
-    static JTextField ackPortField = new JTextField("");
-    static JTextField dataPortField = new JTextField("");
-    static JTextField fileNameField = new JTextField("");
+    static JTextField IPField = new JTextField("127.0.0.1");
+    static JTextField ackPortField = new JTextField("400");
+    static JTextField dataPortField = new JTextField("401");
+    static JTextField fileNameField = new JTextField("Server.java");
     static JTextArea dataArea = new JTextArea("");
+    static JTextField mdsField = new JTextField("1024");
+    static JLabel mdsLabel = new JLabel("Max Datagram Size");
+    static JTextField timeoutField = new JTextField("500");
+    static JLabel timeoutLabel = new JLabel("Timeout (ms)");
     static File file_name;
     static double num_byte;
     static int mds = 1024;
@@ -41,16 +47,17 @@ public class Server extends JFrame implements ActionListener {
     static FileInputStream fileIn;
     static int timeout;
 
-    public Server(){
+    public Server() {
         this.setLocation(200, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         connectPanelInit();
     }
+
     public void connectPanelInit() {
         add(connectPanel);
         connectPanel.setLayout(null);
-        this.setSize(270, 170);
+        this.setSize(300, 200);
         this.setTitle("Connect");
 
         connectPanel.add(connectButton);
@@ -74,13 +81,13 @@ public class Server extends JFrame implements ActionListener {
         dataPortField.setBounds(20, 80, 100, 25);
         dataPortField.setVisible(true);
 
-        IPLabel.setBounds(130, 10, 80, 20);
+        IPLabel.setBounds(130, 10, 110, 20);
         IPLabel.setVisible(true);
 
-        ackPortLabel.setBounds(20, 10, 80, 20);
+        ackPortLabel.setBounds(20, 10, 110, 20);
         ackPortLabel.setVisible(true);
 
-        dataPortLabel.setBounds(20, 60, 80, 20);
+        dataPortLabel.setBounds(20, 60, 110, 20);
         dataPortLabel.setVisible(true);
 
         connectButton.addActionListener(this);
@@ -89,118 +96,132 @@ public class Server extends JFrame implements ActionListener {
     public void clientPanelInit() throws Exception {
         add(clientPanel);
         clientPanel.setLayout(null);
-        this.setSize(575, 400);
+        this.setSize(475, 300);
+        clientPanel.setSize(475, 300);
         this.setTitle("Client");
 
         clientPanel.setLayout(null);
         clientPanel.add(disconnectButton);
+        clientPanel.add(transferButton);
+        clientPanel.add(dataArea);
+        clientPanel.add(fileNameField);
+        clientPanel.add(mdsField);
+        clientPanel.add(mdsLabel);
+        clientPanel.add(timeoutField);
+        clientPanel.add(timeoutLabel);
+
         clientPanel.setVisible(true);
 
-        clientPanel.add(dataArea);
-        dataArea.setBounds(10, 200, 350, 150);
+        dataArea.setBounds(10, 100, 250, 150);
         dataArea.setVisible(true);
         dataArea.setEditable(false);
         dataArea.setLineWrap(true);
 
-        clientPanel.add(fileNameField);
         fileNameField.setVisible(true);
-        fileNameField.setBounds();
-        
-        disconnectButton.setBounds(380, 320, 170, 30);
+        fileNameField.setBounds(10, 10, 80, 30);
+
+        transferButton.setVisible(true);
+        transferButton.setBounds(100, 10, 160, 30);
+
+        mdsLabel.setVisible(true);
+        mdsLabel.setBounds(280, 10, 110, 20);
+
+        mdsField.setVisible(true);
+        mdsField.setBounds(280, 30, 110, 20);
+
+        timeoutLabel.setVisible(true);
+        timeoutLabel.setBounds(280, 60, 110, 20);
+
+        timeoutField.setVisible(true);
+        timeoutField.setBounds(280, 80, 110, 20);
+
+
+
+        disconnectButton.setBounds(280, 120, 170, 30);
         disconnectButton.setVisible(true);
 
         disconnectButton.addActionListener(this);
+        transferButton.addActionListener(this);
     }
 
-    static Thread in = new Thread() {
-        public void run() {
-            try {
-                boolean fileTransfering = true;
-                while (fileTransfering) {
-                    fileTransfering = false;
-                    for (int i = 0; i < num_seq; i++) {
-                        if (acknowleged[i] != 1) {
-
-                            byte[] send = new byte[4 + mds];
-                            byte[] result = new byte[] { (byte) (i >> 24), (byte) (i >> 16), (byte) (i >> 8),
-                                    (byte) i };
-
-                            System.arraycopy(result, 0, send, 0, 4);
-                            System.arraycopy(packages[i], 0, send, 4, mds);
-                            DatagramPacket n = new DatagramPacket(send, send.length);
-                            sock.send(n);
-
-                            ByteBuffer wrapped = ByteBuffer.wrap(Arrays.copyOfRange(send, 0, 4)); // big-endian by
-                                                                                                  // default
-                            int num = wrapped.getInt();
-                            System.out.println(num);
-                            System.out.println(new String(Arrays.copyOfRange(send, 4, send.length)));
-                            fileTransfering = true;
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println(e + "here");
-            }
-        }
-    };
     static Thread out = new Thread() {
         public void run() {
             try {
-                while (true) {
+                byte[] handshake = (Integer.toString(mds + 4) + " " + Integer.toString(num_seq)).getBytes();
+                DatagramPacket hand = new DatagramPacket(handshake, handshake.length);
+                sock.send(hand);
+                boolean fileTransfering = true;
+                while (acknowleged[num_seq - 1] != 1) {
+                    try{
+                    int i = 0;
+
+                    byte[] send = new byte[4 + mds];
+                    byte[] result = new byte[] { (byte) (i >> 24), (byte) (i >> 16), (byte) (i >> 8), (byte) i };
+
+                    System.arraycopy(result, 0, send, 0, 4);
+                    System.arraycopy(packages[i], 0, send, 4, mds);
+                    DatagramPacket n = new DatagramPacket(send, send.length);
+                    sock.send(n);
+
+                    fileTransfering = true;
+
                     byte[] buf = new byte[mds];
-                    DatagramPacket n = new DatagramPacket(buf, mds);
-                    sock.receive(n);
+                    DatagramPacket ack = new DatagramPacket(buf, 4);
+                    sock.receive(ack);
                     int seq = java.nio.ByteBuffer.wrap(buf).getInt();
                     acknowleged[seq] = 1;
-                    System.out.println(new String(buf));
+                    i++;
+                    }
+                    catch(SocketTimeoutException h){
+                        System.out.println(h);
+                    }
+
                 }
+
             } catch (Exception e) {
-                System.out.println(e);
+                System.out.println(e );
             }
         }
-
     };
 
     public static void main(String[] args) {
         new Server();
-        try {
-            for (int i = 0; i < num_seq; i++) {
-                acknowleged[i] = 0;
-                fileIn.read(packages[i]);
-            }
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try{
-        String action = e.getActionCommand();
-        if (action.equals("CONNECT")){
-        portIn = Integer.parseInt(dataPortField.getText());
-        endPort = Integer.parseInt(ackPortField.getText());
-        endIP = InetAddress.getByName(IPField.getText());
-        sock = new DatagramSocket(portIn);
-        fileIn = new FileInputStream(file_name);
-        sock.connect(endIP, endPort);
-        connectPanel.setVisible(false);
-        clientPanelInit();
+        try {
+            String action = e.getActionCommand();
+            if (action.equals("CONNECT")) {
+                portIn = Integer.parseInt(dataPortField.getText());
+                endPort = Integer.parseInt(ackPortField.getText());
+                endIP = InetAddress.getByName(IPField.getText());
+
+                sock = new DatagramSocket(portIn);
+                sock.connect(endIP, endPort);
+                connectPanel.setVisible(false);
+                clientPanelInit();
+
+            } else if (action.equals("TRANSFER")) {
+                file_name = new File(fileNameField.getText());
+                fileIn = new FileInputStream(file_name);
+                num_byte = file_name.length();
+                mds = Integer.parseInt(mdsField.getText());
+                num_seq = (int) Math.ceil(num_byte / mds);
+                timeout = Integer.parseInt(timeoutField.getText());
+                sock.setSoTimeout(timeout);
+                packages = new byte[num_seq][mds];
+                acknowleged = new int[num_seq];
+                for (int i = 0; i < num_seq; i++) {
+                    acknowleged[i] = 0;
+                    fileIn.read(packages[i]);
+                }
+                out.start();
+
+            }
+        } catch (Exception ae) {
+            System.out.println(ae);
         }
-        else if (action.equals("TRANSFER")){
-            file_name = new File("Server.java");
-            num_byte = file_name.length();
-            mds = 1024;
-            num_seq = (int) Math.ceil(num_byte / mds);
-            packages = new byte[num_seq][mds];
-            acknowleged = new int[num_seq];
-            in.run();
-            out.run();
-        }
-    }catch(Exception ae){
-        System.out.println(ae);
-    }
     }
 }
